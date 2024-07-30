a07565c234465a599eab2298bb6270198594f4f9
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-07-31 07:02:29 +0900

    #7 chore: hiltの依存関係を追加

---
+++ app/src/main/java/com/example/taskmaster/screen/home/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.taskmaster.screen.home
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
+}
