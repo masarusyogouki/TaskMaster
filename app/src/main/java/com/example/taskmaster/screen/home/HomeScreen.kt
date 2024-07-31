af4a8bc5fd429d9b47a5c072efd9fd04da6e21da
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-07-31 09:41:45 +0900

    #7 feat: HomeScreen追加

--- app/src/main/java/com/example/taskmaster/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/taskmaster/screen/home/HomeScreen.kt
@@ -1,8 +1,34 @@
 package com.example.taskmaster.screen.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
+    HomeScreenContent(
+        onAddClick = viewModel::addTask,
+    )
+}
+
+@Composable
+fun HomeScreenContent(
+    modifier: Modifier = Modifier,
+    onAddClick: (String) -> Unit,
+) {
+    Column(
+        modifier =
+            modifier
+                .fillMaxSize()
+                .fillMaxHeight()
+                .verticalScroll(rememberScrollState()),
+    ) {
+        Text(text = "Home Screen")
+    }
 }
