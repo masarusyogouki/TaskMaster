636340b95fdac773766faca61c6f82f028931ed2
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-08-01 14:36:05 +0900

    feat: TaskCardを追加

---
+++ app/src/main/java/com/example/taskmaster/common/composable/CardComposable.kt
@@ -0,0 +1,70 @@
+package com.example.taskmaster.common.composable
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.sharp.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.taskmaster.R
+import com.example.taskmaster.model.Priority
+
+@Composable
+fun TaskCard(
+    isCompleted: Boolean,
+    title: String,
+    priority: Priority,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+    ) {
+        Icon(
+            painter =
+                if (isCompleted) {
+                    painterResource(id = R.drawable.expand_circle_down)
+                } else {
+                    painterResource(id = R.drawable.radio_button_unchecked)
+                },
+            contentDescription = "isCompleted",
+            modifier = Modifier.size(24.dp),
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = title,
+            modifier = Modifier.weight(1f),
+        )
+
+        Icon(
+            imageVector =
+                if (priority == Priority.None) {
+                    Icons.Sharp.Star
+                } else {
+                    Icons.Filled.Star
+                },
+            contentDescription = "priority",
+            tint =
+                if (priority == Priority.None) {
+                    Color.Gray.copy(alpha = 0.3f)
+                } else {
+                    Color.Yellow
+                },
+            modifier = Modifier.size(24.dp),
+        )
+    }
+}
