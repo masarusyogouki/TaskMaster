e1ce756542a97bff4606c25741951c193e9f1442
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-08-05 13:47:28 +0900

    feat: TaskRepositoryを更新

--- app/src/main/java/com/example/taskmaster/model/repository/TaskRepository.kt
+++ app/src/main/java/com/example/taskmaster/model/repository/TaskRepository.kt
@@ -3,19 +3,30 @@ package com.example.taskmaster.model.repository
 import com.example.taskmaster.model.Priority
 import com.example.taskmaster.model.Task
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 
 interface TaskRepository {
     suspend fun addTask(task: Task): Long
 
     fun getTasks(): Flow<List<Task>>
 
-    suspend fun updateTaskCompleted(
+    suspend fun updateTaskTitle(
         taskId: Long,
-        isCompleted: Boolean,
+        title: String,
+    )
+
+    suspend fun updateTaskDueDate(
+        taskId: Long,
+        dueDate: LocalDate,
     )
 
     suspend fun updateTaskPriority(
         taskId: Long,
         priority: Priority,
+    )
+
+    suspend fun updateTaskCompleted(
+        taskId: Long,
+        isCompleted: Boolean,
     )
 }
--- app/src/main/java/com/example/taskmaster/model/repository/impl/TaskRepositoryImpl.kt
+++ app/src/main/java/com/example/taskmaster/model/repository/impl/TaskRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.example.taskmaster.model.Task
 import com.example.taskmaster.model.data.TaskDao
 import com.example.taskmaster.model.repository.TaskRepository
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 import javax.inject.Inject
 
 class TaskRepositoryImpl
@@ -16,13 +17,23 @@ class TaskRepositoryImpl
 
         override fun getTasks(): Flow<List<Task>> = taskDao.getALLTasks()
 
-        override suspend fun updateTaskCompleted(
+        override suspend fun updateTaskTitle(
             taskId: Long,
-            isCompleted: Boolean,
-        ) = taskDao.updateTaskCompleted(taskId, isCompleted)
+            title: String,
+        ) = taskDao.updateTaskTitle(taskId, title)
+
+        override suspend fun updateTaskDueDate(
+            taskId: Long,
+            dueDate: LocalDate,
+        ) = taskDao.updateTaskDueDate(taskId, dueDate)
 
         override suspend fun updateTaskPriority(
             taskId: Long,
             priority: Priority,
         ) = taskDao.updateTaskPriority(taskId, priority)
+
+        override suspend fun updateTaskCompleted(
+            taskId: Long,
+            isCompleted: Boolean,
+        ) = taskDao.updateTaskCompleted(taskId, isCompleted)
     }
