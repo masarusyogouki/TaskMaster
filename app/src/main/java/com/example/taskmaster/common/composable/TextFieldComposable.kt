d984080a40464dba5629f8eb8a629a0e91b76c87
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-12-27 20:05:51 +0900

    feat: コンポーネントの実装

--- app/src/main/java/com/example/taskmaster/common/composable/TextFieldComposable.kt
+++ app/src/main/java/com/example/taskmaster/common/composable/TextFieldComposable.kt
@@ -9,19 +9,33 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.taskmaster.R
 import com.example.taskmaster.ui.theme.DarkGray
 import com.example.taskmaster.ui.theme.MilkyWhite
 import com.example.taskmaster.ui.theme.PaleBlue
+
 
 @Composable
 fun NewTaskField(
@@ -33,10 +47,10 @@ fun NewTaskField(
     TextField(
         singleLine = true,
         modifier =
-            modifier
-                .padding(8.dp)
-                .height(60.dp)
-                .fillMaxWidth(),
+        modifier
+            .padding(8.dp)
+            .height(60.dp)
+            .fillMaxWidth(),
         value = value,
         onValueChange = { onNewValue(it) },
         placeholder = {
@@ -96,5 +110,66 @@ fun BasicTextField(
                 focusedIndicatorColor = MilkyWhite,
                 unfocusedIndicatorColor = MilkyWhite,
             ),
+    )
+}
+
+@Composable
+fun EmailTextField(
+    value: String,
+    onNewValue: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        singleLine = true,
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = { onNewValue(it) },
+        placeholder = { Text("メールアドレスを入力してください")},
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "Email")}
+    )
+}
+
+@Composable
+fun PasswordTextField(value: String, onNewValue: (String) -> Unit, modifier: Modifier = Modifier) {
+    PasswordField(value, onNewValue, modifier)
+}
+
+@Composable
+fun RepeatPasswordTextField(
+    value: String,
+    onNewValue: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    PasswordField(value, onNewValue, modifier)
+}
+
+@Composable
+fun PasswordField(
+    value: String,
+    onNewValue: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var isVisible by remember { mutableStateOf(false) }
+
+    val icon =
+        if (isVisible) painterResource(R.drawable.ic_visibility_on)
+        else painterResource(R.drawable.ic_visibility_off)
+
+    val visualTransformation =
+        if (isVisible) VisualTransformation.None else PasswordVisualTransformation()
+
+    OutlinedTextField(
+        modifier = modifier.fillMaxWidth(),
+        value = value,
+        onValueChange = { onNewValue(it) },
+        placeholder = { Text("パスワードを入力してください")},
+        leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock")},
+        trailingIcon = {
+            IconButton(onClick = { isVisible = !isVisible}) {
+                Icon(painter = icon, contentDescription = "Visibility")
+            }
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        visualTransformation = visualTransformation
     )
 }
