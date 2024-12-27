b8e2730afca2391bc1ef613937c35007ad7e7e67
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-12-27 21:42:23 +0900

    refactor: ktlintのフォーマットに従ったコードに変更

--- app/src/main/java/com/example/taskmaster/common/ext/StringExt.kt
+++ app/src/main/java/com/example/taskmaster/common/ext/StringExt.kt
@@ -6,20 +6,11 @@ import java.util.regex.Pattern
 private const val MIN_PASS_LENGTH = 6
 private const val PASS_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{4,}$"
 
-fun String.isValidEmail(): Boolean {
-    return this.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
-}
+fun String.isValidEmail(): Boolean = this.isNotBlank() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
 
-fun String.isValidPassword(): Boolean {
-    return this.isNotBlank() &&
+fun String.isValidPassword(): Boolean =
+    this.isNotBlank() &&
         this.length >= MIN_PASS_LENGTH &&
         Pattern.compile(PASS_PATTERN).matcher(this).matches()
-}
 
-fun String.passwordMatches(repeated: String): Boolean {
-    return this == repeated
-}
-
-fun String.idFromParameter(): String {
-    return this.substring(1, this.length - 1)
-}
+fun String.passwordMatches(repeated: String): Boolean = this == repeated
