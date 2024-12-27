94f82c5e3a604b043115c52b7adbd2b8e1ff9642
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2024-12-27 20:05:06 +0900

    feat: 認証関連のmodelを開発

---
+++ app/src/main/java/com/example/taskmaster/model/repository/AuthRepository.kt
@@ -0,0 +1,16 @@
+package com.example.taskmaster.model.repository
+
+import com.example.taskmaster.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface AuthRepository {
+    val currentUserId: String
+    val hasUser: Boolean
+
+    val currentUser: Flow<User>
+
+    suspend fun register(email: String, password: String)
+    suspend fun signIn(email: String, password: String)
+    suspend fun logOut()
+    suspend fun sendRecoveryEmail(email: String)
+}
